8c7d14b3bc0ece92c650cdaf56114c7c64ee72b0
Fola Aruleba <folaaruleba@Folas-Air.home>
2022-05-22 20:12:53 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { createContext, useState } from "react";
+import { boardDefault } from "./Words";
+
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0})
+
+
+  const correrctWord = "RIGHT";
+
+
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt({...currAttempt, letterPos : currAttempt.letterPos + 1 });
+  
+  };
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+ setBoard(newBoard)
+ setCurrAttempt({...currAttempt, letterPos:currAttempt.letterPos - 1 })
+
+  };
+
+  const onEnter = () => {
+    if(currAttempt.letterPos !== 5) return;
+setCurrAttempt({attempt: currAttempt.attempt + 1, letterPos: 0});
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <nav>
+    <h1>Wordle</h1>
+    </nav>
+    <AppContext.Provider value={{
+       board, 
+       setBoard,
+        currAttempt, 
+        setCurrAttempt, 
+        onSelectLetter, 
+        onDelete, 
+        onEnter ,
+        correrctWord,
+        }}>
+    <div className="game">
+    <Board />
+    <Keyboard />
+    </div>
+    </AppContext.Provider>
     </div>
   );
-}
 
+<a>
+  className = "App-link"
+  href="https://github.com/maruleba11/maruleba11"
+</a>
+}
 export default App;
